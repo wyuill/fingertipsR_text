2bc799eecc7283782b87dcf0a91af80e6d5285ae
wyuill <10546300+wyuill@users.noreply.github.com>
2018-11-25 22:06:03 +0000

    new function to create sentences

---
+++ createsentence.R
@@ -0,0 +1,46 @@
+# The VALUETYPE for AREANAME in TIMEPERIOD was VALUE.
+# The recent trend is RECENTREND.
+# Compared to the goal AREANAME was COMPAREDTOGOAL.
+# This is COMPAREDTOENGLAND than England.
+# This is COMPAREDTOREGION than the Region.
+# Compared to GEOTYPE, AREANAME was statistically significantly POLARITY than
+#   count(_HIGHER) (_HIGHER) and statistically significantly POLARITY then
+#   count(_LOWER) (_LOWER)
+#        **REPEAT LAST AS MANY TIMES AS SPECIFIED WITH DIFFERENT AREAS**
+# Definitions: 95% CI Compare area lists
+
+# Ideas: order of sentences, add category, add values of other areas (e.g. England)
+
+textanalysis <- function(data, trend = TRUE, country = TRUE,
+                         region = TRUE, goal = TRUE, definitions = TRUE) {
+  
+  value_txt <- paste0("The ", "VALUETYPE", " of ", data$IndicatorName, " for ",
+                      tolower(data$Sex), "s aged ", data$Age, " in ", data$AreaName, " in ",
+                      data$Timeperiod, " was ", round(data$Value, 2), ". ")
+  
+  trend_txt <- if (trend == FALSE) {""} else {
+    if (is.na(data$RecentTrend)) {""} else {
+    paste0("The recent trend is ", data$RecentTrend, ". ")
+  } }
+  
+  goal_txt <-  if (goal == FALSE) {""} else {
+    if (is.na(data$Comparedtogoal)) {""} else {
+    paste0(data$AreaName, " is performing ", tolower(data$Comparedtogoal), " than the goal. ")
+  } }
+  
+  country_txt <-  if (country == FALSE) {""} else {
+    if (is.na(data$ComparedtoEnglandvalueorpercentiles)) {""} else {
+    paste0("This is statistically ", tolower(data$ComparedtoEnglandvalueorpercentiles), " than England. ")
+  } }
+  
+  region_txt <- if (region == FALSE) {""} else {
+    if (is.na(data$ComparedtoRegionvalueorpercentiles)) {""} else {
+    paste0("This is statistically ", tolower(data$ComparedtoRegionvalueorpercentiles), " to the Region. ")
+  } }
+  
+  #comp_txt <- 
+  
+  combined = paste0(value_txt, trend_txt, goal_txt, country_txt, region_txt, comp_txt)
+  
+}
+
--- testing.R
+++ testing.R
@@ -3,7 +3,7 @@ p_load(fingertipsR, dplyr, stringr)
 
 # Set parameters examples
 p_area <- "E10000015"
-inds <- c(90366, 108)
+inds <- c(90630, 108)
 c_areas <- nearest_neighbours(p_area, 102, "CIPFA")
 district <- c("E07000095","E07000096","E07000098","E07000099","E07000102","E07000103","E07000240",
               "E07000241","E07000242","E07000243")
@@ -12,10 +12,11 @@ district <- c("E07000095","E07000096","E07000098","E07000099","E07000102","E0700
 data1 <- fingertips_data(inds, AreaTypeID = 102)
 #data2 <- fingertips_data(inds, AreaCode = district, AreaTypeID = 101, ParentAreaTypeID = 102)
 
-tesmp <- data1[1]
+test <- comparetoareas(data1, p_area, c_areas, "CIPFA")
 
-t <- comparetoareas(data1, p_area, c_areas, "CIPFA")
-y <- comparetoareas(data1, tesmp, tesmp)
+test1 <- textanalysis(test)
+
+test1[1]
 
 # Need to add:
 # Create sentence for compare areas
